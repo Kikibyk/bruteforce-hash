eba2f2835e1376559e0b85d2e62b2ac044c4fe88
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2021-03-28 16:30:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ let end = 0;
 if(!process.argv[2]) return console.error(`You have not specified a hash!\nnode app.js [hash]`);
 
 console.log(`\x1b[36m> \x1b[0mBrute force started..`);
+require('fs').readdir("./dictionary/", (err, files) => {
+    for(let i = 1; i < files.length+1; i++) {
+        let worker = new Worker(`./worker.js`, {workerData: `${process.argv[2]} ${i}`});
+        worker.on('message', (data) => {
+            if(!data.success) end++;
+            else {
+                console.log(`\x1b[32m> \x1b[0mPassword found: ${data.password}`);
+                process.exit();
+            }
 
-for(let i = 1; i < 18; i++) {
-    let worker = new Worker(`./worker.js`, {workerData: `${process.argv[2]} ${i}`});
-    worker.on('message', (data) => {
-        if(!data.success) end++;
-        else {
-            console.log(`\x1b[32m> \x1b[0mPassword found: ${data.password}`);
-            process.exit();
-        }
-
-        if(end === 17 && !data.success) {
-            console.log(`\x1b[31m> \x1b[0mPassword not found\n`);
-            process.exit();
-        }
-    });
-}
+            if(end === 17 && !data.success) {
+                console.log(`\x1b[31m> \x1b[0mPassword not found\n`);
+                process.exit();
+            }
+        });
+    }
+})
